a6807ae0f18a03d4e71eab80a87784dea4c7bf31
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-21 10:39:13 +0000

    Added footer links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://portfolio-georgia-lee.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Georgia Lee
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/georgia-lee04/react-project-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
