02d6d2ecb96320b6fd8e35bea6de5cd6a7997107
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-21 11:09:06 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://portfolio-georgia-lee.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Georgia Lee
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/georgia-lee04/react-project-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://portfolio-georgia-lee.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Georgia Lee
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/georgia-lee04/react-project-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
