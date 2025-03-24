c31861a0fea73a57f3ea22b22953cee352d8d5ef
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-24 11:16:16 +0000

    Included both celsius and fahrenheit units

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>{" "}
-          <span className="unit">°C</span>
+
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
